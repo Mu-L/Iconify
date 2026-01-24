c9eedcd905e3bcb455af73f641e7647425e6bda9
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2026-01-24 21:47:28 +0600

    Update build.gradle.kts to use kotlin compilerOptions for jvmTarget
    Signed-off-by: DrDisagree <29881338+Mahmud0808@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import java.io.FileInputStream
 import java.util.Properties
 
@@ -120,8 +121,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
     }
 
     packaging {
