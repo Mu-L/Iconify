d6749b7b63c8e6322fa509a18fb7d1cb353da58b
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2026-04-06 14:03:02 +0600

    Remove category title from QsMarginsScreen

--- app/src/main/java/com/drdisagree/iconify/features/xposed/quicksettings/margins/screens/QsMarginsScreen.kt
+++ app/src/main/java/com/drdisagree/iconify/features/xposed/quicksettings/margins/screens/QsMarginsScreen.kt
@@ -14,7 +14,7 @@ import com.drdisagree.iconify.data.keys.XposedKey
 import com.drdisagree.iconify.features.common.viewmodels.SystemActionViewModel
 
 val qsMarginsPreferences = preferenceScreen {
-    category(title = stringRes(R.string.section_title_transparency)) {
+    category {
         switch(
             key = XposedKey.CUSTOM_QS_MARGINS,
             isMasterSwitch = true,
